30b42fa8ba491487d404c58a05f1d658327c4cd2
fillinmar <mar.nv17@yandexl.com>
2022-03-24 14:38:05 +0300

    lab2

--- app/src/main/java/ru/iu3/fclientmarina/MainActivity.java
+++ app/src/main/java/ru/iu3/fclientmarina/MainActivity.java
@@ -1,49 +1,75 @@
 package ru.iu3.fclientmarina;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
-import java.nio.charset.StandardCharsets;
-
+import java.util.Random;
 import ru.iu3.fclientmarina.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'fclientmarina' library on application startup.
     static {
         System.loadLibrary("native-lib");
         System.loadLibrary("mbedcrypto");
     }
 
     private ActivityMainBinding binding;
+    ActivityResultLauncher activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         int res = initRng();
-        //        для примера, зашифруем и расшифруем что то с помощью алгоритма 3DES
-        byte[] secretKey = randomBytes(16);
-//
-        byte[] secret = "ILOVEBMSTU".getBytes();
-        byte[] encryption = encrypt(secretKey, secret);
+        byte[] d_key = randomBytes(16);
+        Random random = new Random();
+        byte[] mas = new byte[20];
+        for (int i = 0; i < mas.length; ++i) {
+            mas[i] = (byte) ((byte) random.nextInt() % 255);
+        }
+        byte[] encrypt_mas = encrypt(d_key, mas);
+        byte[] decrypt_mas = decrypt(d_key, encrypt_mas);
 
-        byte[] decryption = decrypt(secretKey, encryption);
+        activityResultLauncher =
+                registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-        String secretWord = new String(decryption, StandardCharsets.UTF_8);
-
-        setContentView(R.layout.activity_main);
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
 
     }
 
-    /**
-     * A native method that is implemented by the 'fclientmarina' native library,
-     * which is packaged with this application.
-     */
+    public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //при StartActivity нет вывода сообщения. Используем  activityResultLauncher
+        activityResultLauncher.launch(it);
+//        startActivity(it);
+    }
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
