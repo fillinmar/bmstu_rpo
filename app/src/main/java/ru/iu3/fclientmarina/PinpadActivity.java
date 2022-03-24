7181d9ca286167b930e659d6195060c86d759b75
fillinmar <mar.nv17@yandexl.com>
2022-03-24 14:27:08 +0300

    lab2

---
+++ app/src/main/java/ru/iu3/fclientmarina/PinpadActivity.java
@@ -0,0 +1,14 @@
+package ru.iu3.fclientmarina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PinpadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pinpad2);
+    }
+}
